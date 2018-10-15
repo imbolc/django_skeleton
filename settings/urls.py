861e2234132b6d8d8a21fa4eda508a3ca5e0294f
imbolc <imbolc@imbolc.name>
2018-10-15 21:19:22 +0700

    Up

--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+    path('accounts/login/', LoginView.as_view(
+        template_name='admin/login.html',
+        extra_context={'site_header': 'Skeleton'}),
+        name='login'),
     path('admin/', admin.site.urls),
 ]
